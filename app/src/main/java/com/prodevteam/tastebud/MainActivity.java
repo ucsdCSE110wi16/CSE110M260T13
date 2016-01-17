cc52dc7a1c859a42e943e11c0f962e9184b5986d
Belton Zhong <byzhong@ucsd.edu>
2016-01-16 20:07:06 -0800

    Created a Login Screen Activity.

--- app/src/main/java/com/prodevteam/tastebud/MainActivity.java
+++ app/src/main/java/com/prodevteam/tastebud/MainActivity.java
@@ -4,16 +4,50 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.RelativeLayout;
 
 public class MainActivity extends ActionBarActivity {
 
+    private int imageIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        imageIndex = 1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    changeBackgroundImage();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        }).run();
     }
 
+    private int changeBackgroundImage() {
+        RelativeLayout loginScreen = (RelativeLayout) findViewById(R.id.login_screen);
+        imageIndex = (imageIndex % 4) + 1;
+        switch(imageIndex) {
+            case 1:
+                loginScreen.setBackground(getResources().getDrawable(R.drawable.login_1));
+                break;
+            case 2:
+                loginScreen.setBackground(getResources().getDrawable(R.drawable.login_2));
+                break;
+            case 3:
+                loginScreen.setBackground(getResources().getDrawable(R.drawable.login_3));
+                break;
+            case 4:
+                loginScreen.setBackground(getResources().getDrawable(R.drawable.login_4));
+        }
+        return 1;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

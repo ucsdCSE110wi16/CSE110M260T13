4b979a5fdf483a2f59d7d0b9627e5c2b10d609b8
Justin N. Nwaokoro <nwaokoron@gmail.com>
2016-03-07 11:58:32 -0800

    fix bugs from last push

--- app/src/androidTest/java/com/prodevteam/tastebud/zAddRestictionsTest.java
+++ app/src/androidTest/java/com/prodevteam/tastebud/zAddRestictionsTest.java
@@ -1,4 +1,21 @@
 package com.prodevteam.tastebud;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
+
 
 /**
  * Created by Super on 3/7/16.
